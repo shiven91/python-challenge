210ff03867d331c81349b9c927ff813b11d2a1de
Siven Patel <sivenpatel@Sivens-MBP.fios-router.home>
2018-02-19 20:41:49 -0500

    changed the path of the file

--- pybank/main.py
+++ pybank/main.py
@@ -7,33 +7,38 @@ Created on Mon Feb 19 16:07:22 2018
 """
 
 import csv
+import os
 
-previousChange = []
+csvFiles = ["budget_data_1.csv"]
 
-with open ('budget_data_1.csv', 'r') as cfile:
-    csv_reader = csv.reader(cfile)
-    next (csv_reader)
+for file in csvFiles:
+    csvPath = os.path.join("raw_data",file)
+    previousChange = []
     date = []
     revenue = []
+
+    with open (csvPath, newline='') as cfile:
+        csv_reader = csv.reader(cfile, delimiter=',')
+        next (csv_reader)
     
-    for row in csv_reader:
-        date.append(row[0])
-        #print(date)
-        revenue.append(int(row[1]))
-        #print(revenue)
+        for row in csv_reader:
+            date.append(row[0])
+            #print(date)
+            revenue.append(int(row[1]))
+            #print(revenue)
     
     
-    for i in range(1,len(revenue)):
-        previousChange.append(revenue[i]-revenue[i-1])
-        #print (previousChange)
-        avgRevChange = sum(previousChange)/len(previousChange)
-        #print (avgRevChange)
-        maxRevChange = max(previousChange)
-        #print (maxRevChange)
-        maxRevChangeDate = str(date[previousChange.index(maxRevChange)])
-        
-        minRevChange = min(previousChange)
-        minRevChangeDate = str(date[previousChange.index(minRevChange)])
+        for i in range(1,len(revenue)):
+            previousChange.append(revenue[i]-revenue[i-1])
+            #print (previousChange)
+            avgRevChange = sum(previousChange)/len(previousChange)
+            #print (avgRevChange)
+            maxRevChange = max(previousChange)
+            #print (maxRevChange)
+            maxRevChangeDate = str(date[previousChange.index(maxRevChange)])
+    
+            minRevChange = min(previousChange)
+            minRevChangeDate = str(date[previousChange.index(minRevChange)])
 
         
 print ()
