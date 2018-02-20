367142627b15124fabdcb1c4e1039a0d271ae39a
Siven Patel <sivenpatel@Sivens-MBP.fios-router.home>
2018-02-19 20:15:43 -0500

    pybank finished

---
+++ pybank/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 16:07:22 2018
+
+@author: sivenpatel
+"""
+
+import csv
+
+previousChange = []
+
+with open ('budget_data_1.csv', 'r') as cfile:
+    csv_reader = csv.reader(cfile)
+    next (csv_reader)
+    date = []
+    revenue = []
+    
+    for row in csv_reader:
+        date.append(row[0])
+        #print(date)
+        revenue.append(int(row[1]))
+        #print(revenue)
+    
+    
+    for i in range(1,len(revenue)):
+        previousChange.append(revenue[i]-revenue[i-1])
+        #print (previousChange)
+        avgRevChange = sum(previousChange)/len(previousChange)
+        #print (avgRevChange)
+        maxRevChange = max(previousChange)
+        #print (maxRevChange)
+        maxRevChangeDate = str(date[previousChange.index(maxRevChange)])
+        
+        minRevChange = min(previousChange)
+        minRevChangeDate = str(date[previousChange.index(minRevChange)])
+
+        
+print ()
+print ()
+print ("Financial Analysis")
+print ("-------------------")
+print ()
+print ("Total Months: ", len(date))
+print ("Total Revenue: $",sum(revenue))
+print ("Average Revenue Change: $",avgRevChange)
+print ("Greatest Increase in Revenue: "+ 
+       str(maxRevChangeDate) + "($" + str(maxRevChange) +")")
+print ("Greatest Decrease in Revenue: "+ 
+       str(minRevChangeDate) + "($" + str(minRevChange) +")")
+
