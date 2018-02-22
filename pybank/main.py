530f6f94b794dba05383ade2b10d5f4053cf67a9
Siven Patel <sivenpatel@Sivens-MBP.fios-router.home>
2018-02-21 23:24:15 -0500

    added code for creating a file

--- pybank/main.py
+++ pybank/main.py
@@ -9,15 +9,15 @@ Created on Mon Feb 19 16:07:22 2018
 import csv
 import os
 
-csvFiles = ["budget_data_1.csv"]
+csvFile = ["budget_data_1.csv"]
 
-for file in csvFiles:
+for file in csvFile:
     csvPath = os.path.join("raw_data",file)
-    previousChange = []
+    previousChange = [1]
     date = []
     revenue = []
 
-    with open (csvPath, newline='') as cfile:
+    with open (csvPath, newline="") as cfile:
         csv_reader = csv.reader(cfile, delimiter=',')
         next (csv_reader)
     
@@ -31,26 +31,46 @@ for file in csvFiles:
         for i in range(1,len(revenue)):
             previousChange.append(revenue[i]-revenue[i-1])
             #print (previousChange)
-            avgRevChange = sum(previousChange)/len(previousChange)
+            avgRevChange = round(sum(previousChange)/len(previousChange))
             #print (avgRevChange)
             maxRevChange = max(previousChange)
             #print (maxRevChange)
+            minRevChange = min(previousChange)
+            #print (minRevChange)
+            
             maxRevChangeDate = str(date[previousChange.index(maxRevChange)])
     
-            minRevChange = min(previousChange)
+            
             minRevChangeDate = str(date[previousChange.index(minRevChange)])
 
-        
-print ()
-print ()
-print ("Financial Analysis")
-print ("-------------------")
-print ()
-print ("Total Months: ", len(date))
-print ("Total Revenue: $",sum(revenue))
-print ("Average Revenue Change: $",avgRevChange)
-print ("Greatest Increase in Revenue: "+ 
-       str(maxRevChangeDate) + "($" + str(maxRevChange) +")")
-print ("Greatest Decrease in Revenue: "+ 
-       str(minRevChangeDate) + "($" + str(minRevChange) +")")
+
+    with open ("Results.txt", "w") as output:
+        print()
+        print()
+        print("The results are for " + str(csvFile[0]))
+        print()
+        print("Financial Analysis")
+        print("-------------------")
+        print("Total Months: ", len(date))
+        print("Total Revenue: $",sum(revenue))
+        print("Average Revenue Change: $",avgRevChange)
+        print("Greatest Increase in Revenue: "+ 
+              str(maxRevChangeDate) + "($" + str(maxRevChange) +")")
+        print("Greatest Decrease in Revenue: "+ 
+              str(minRevChangeDate) + "($" + str(minRevChange) +")")
+        output.write('\n')
+        output.write('\n')
+        output.write('The results are for ' + str(csvFile[0]) + '\n')
+        output.write('\n')
+        output.write('Financial Analysis\n')
+        output.write('-------------------\n')
+        output.write('Total Months: ' + str(len(date)) + '\n')
+        output.write('Total Revenue: $' + str(sum(revenue)) + '\n')
+        output.write('Average Revenue Change: $' + str(avgRevChange) + '\n')
+        output.write('Greatest Increase in Revenue: ' + 
+                     str(maxRevChangeDate) + '($' + str(maxRevChange) + ')\n')
+        output.write('Greatest Decrease in Revenue: ' + 
+                     str(minRevChangeDate) + '($' + str(minRevChange) + ')\n')
+
+    
 
